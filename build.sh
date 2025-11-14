bdac19425ca6980ba7f1c26f531a414c28675237
Onyeka Nwokike <nwokikeonyeka@gmail.com>
2025-11-14 19:36:53 +0100

    Add superuser creation command to build.sh
    Added one-time superuser creation command to build script.

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
+
+# One-time superuser creation (remove this line after your first successful deploy)
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); email='nwokikeonyeka@gmail.com'; User.objects.filter(email=email).exists() or User.objects.create_superuser(email=email, password='Onyeka1@', first_name='Onyeka', last_name='Nwokike')" | python manage.py shell
