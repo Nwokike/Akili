cf329cecffe60812343c51423851a7d291334463
Onyeka Nwokike <nwokikeonyeka@gmail.com>
2025-11-16 21:29:21 +0100

    Clean up build.sh by removing superuser script
    Removed commented-out superuser creation script from build.sh.

--- build.sh
+++ build.sh
@@ -11,7 +11,3 @@ python manage.py collectstatic --no-input
 
 # Apply database migrations
 python manage.py migrate
-
-# One-time superuser creation
-# This checks if the new user exists and creates it if not.
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='onyeka.nwokike.245742@unn.edu.ng').exists() or User.objects.create_superuser('onyeka.nwokike.245742@unn.edu.ng', 'Maikel1@')" | python manage.py shell
